91012a40d70132b7a8f39b0d1f51839f4ed5ee1c
Thanh Doan <doan.teddy@gmail.com>
2023-07-15 20:21:20 -0400

    init commit

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <h4>Made by Thanh Doan</h4>
+      <nav>hehe</nav>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+  return (
+    <header>
+      <h1>Logo</h1>
+      <nav>Nav Bar here</nav>
+      
+    </header>
+  )
+}
+
+
+export default Header;
