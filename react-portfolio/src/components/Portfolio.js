7a6ce511ea078f72e73a9c39e0b0d7c50a24851f
Thanh Doan <doan.teddy@gmail.com>
2023-08-19 21:25:52 -0400

    fixed portfolio images

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,35 +1,42 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import works from "./portfolio.json";
-import "./Portfolio.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import works from './portfolio.json';
+import './Portfolio.css';
+import img1 from '../images/profile.png';
+import img2 from '../images/SyllaShare.png';
+import img3 from '../images/SpyGame.png';
+import img4 from '../images/snake.jpg';
+import img5 from '../images/Front-page.png';
+import img6 from '../images/screenshot3.png';
 
 function Portfolio() {
+  const imgArray = [img1, img2, img3, img4, img5, img6];
   return (
-    <div className="work-cards">
+    <div className='work-cards'>
       {works.map((work, i) => {
         return (
           <Card>
-            <Card.Img variant="top" src={works[i].screenshot} />
+            <Card.Img variant='top' src={imgArray[i]} />
             <Card.Body>
               <Card.Title>{works[i].title}</Card.Title>
               <Card.Text>{works[i].description}</Card.Text>
               {/* Note: put each button inside div if want them on each row*/}
               {works[i].github && (
                 <a
-                  className="link-button"
-                  target="_blank"
+                  className='link-button'
+                  target='_blank'
                   href={works[i].github}
                 >
-                  <Button variant="primary">Github Link Here </Button>
+                  <Button variant='primary'>Github Link Here </Button>
                 </a>
               )}
               {works[i].external && (
                 <a
-                  className="link-button"
-                  target="_blank"
+                  className='link-button'
+                  target='_blank'
                   href={works[i].external}
                 >
-                  <Button variant="primary">Deployed Link Here </Button>
+                  <Button variant='primary'>Deployed Link Here </Button>
                 </a>
               )}
             </Card.Body>
